0dd4b07ac723fc2f034ce1c80ee73d9775beab8c
Reeul <chotnt741@gmail.com>
2022-06-22 01:02:13 +0900

    init sveltekit

---
+++ src/routes/index.svelte
@@ -0,0 +1,28 @@
+<script context="module" lang="ts">
+	export const prerender = true;
+</script>
+
+<script lang="ts">
+</script>
+
+<section class="h-screen">
+	<div class="flex h-full w-full items-center justify-center container mx-auto px-8">
+		<div class="max-w-2xl text-center ">
+			<h1 class="text-3xl sm:text-5xl capitalize tracking-widest lg:text-7xl font-bold">
+				빠른 일정을 위해, baro meet 😃
+			</h1>
+
+			<p class="mt-6 lg:text-lg text-gray-400">달력과 타임테이블을 이용해 스케줄을 공유하세요!</p>
+
+			<div class="mt-8 flex flex-col space-y-3 sm:-mx-2 sm:flex-row sm:justify-center sm:space-y-0">
+				<button
+					class="w-full rounded-md bg-blue-700 px-8 py-4 font-medium capitalize tracking-wide text-white transition-colors duration-200 hover:bg-blue-600 focus:bg-blue-600 focus:outline-none sm:mx-2"
+					>Let's create</button
+				>
+			</div>
+		</div>
+	</div>
+</section>
+
+<style>
+</style>
