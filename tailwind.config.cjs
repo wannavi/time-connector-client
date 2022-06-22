bff92558627400f0188963fd9d400ade25a63aa4
Reeul <chotnt741@gmail.com>
2022-06-22 19:53:20 +0900

    fix: sticky hover on mobile

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {
+			screens: {
+				'hover-hover': { raw: '(hover: hover)' }
+			}
+		}
 	},
 	plugins: []
 };
